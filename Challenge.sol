8456e762e179e3079b6d091cadafd17d05f22f42
Rohit <rk98991439@gmail.com>
2023-03-31 04:49:33 +0530

    testing

---
+++ Challenge.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract SimpleContract {
+    
+    int public myint;
+    uint public myUint;
+    bool public myBool;
+    string public myString;
+
+    function setMyint(int _myint) public {
+        myint = _myint;
+    }
+    
+    function getMyint() public view returns (int) {
+        return myint;
+    }
+    
+    function setMyUint(uint _myUint) public {
+        myUint = _myUint;
+    }
+    
+    function getMyUint() public view returns (uint) {
+        return myUint;
+    }
+    
+    function setMyBool(bool _myBool) public {
+        myBool = _myBool;
+    }
+    
+    function getMyBool() public view returns (bool) {
+        return myBool;
+    }
+    
+    function setMyString(string memory _myString) public {
+        myString = _myString;
+    }
+    
+    function getMyString() public view returns (string memory) {
+        return myString;
+    }
+    
+}
+
