9578c79cf9baac48e9920bd0ad44b06487ed856f
Rohit <rk98991439@gmail.com>
2023-03-31 06:53:33 +0530

    Primitive Data Types in Solidity

---
+++ Challenge_2.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+// gas 147123 Wei
+// gas 378  Gwei
+// gas 378 Ether
+contract Constants {
+
+    address public constant MY_ADDRESS = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+    uint public constant MY_UINT = 123;
+
+}
+// gas 147232 Wei
+// gas 307  Gwei
+// gas 307 Ether
+contract Var {
+
+    address public  MY_ADDRESS = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+}
