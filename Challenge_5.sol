0597c26338e371d91931ffb83437d0dc45e1f048
Rohit <rk98991439@gmail.com>
2023-04-01 11:30:52 +0530

    contract with 4 functions; add, subtract, multiply and divide 2 numbers.

---
+++ Challenge_5.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract Function 
+{
+        uint a ;
+        uint b ;
+        uint sum ;
+    function add()  public
+    {
+    
+    sum = a+b;
+
+    }
+
+    function sub()  public 
+    {
+    sum = a-b;
+    }  
+    
+      function mul()  public 
+    {
+    sum = a*b;
+    }   
+    
+     function div()  public 
+    {
+    sum = a/b;
+    }
+}
+
